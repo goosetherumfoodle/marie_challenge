31e77045699387ebf3bba07adff1c930835ac16e
jesse <goosetherumfoodle@users.noreply.github.com>
2018-08-04 15:46:18 -0400

    adds loop challenges

---
+++ loops.py
@@ -0,0 +1,11 @@
+## MAPPING
+
+# from a list 1-50, build a list with those numbers doubled
+
+# from a list of ['a', 'B', 'C', 'd', 'E', 'F', 'g'], build a list
+# where the cases are flipped (upper to lower, lower to upper)
+
+# from a list ['horse', 'cat', 'trash', 'apriori', 'kevin'],
+# and a dictionary {'cat': 3 , 'button': 'fog', 'trash': 'balloons'}
+# build a new list, based off the first, which each element either the
+# value corresponding to the key in the dictionary, or Nothing
